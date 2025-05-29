62e3d1ce653b5970fa3d48ca01eef007071254fe
Daniel M <daniel.stackmandev@gmail.com>
2025-05-29 18:41:35 +0400

    Create first screen

---
+++ DoctorsTestApp/App/DoctorsTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoctorsTestAppApp.swift
+//  DoctorsTestApp
+//
+//  Created by Dany on 27.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct DoctorsTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainTabView()
+        }
+    }
+}
